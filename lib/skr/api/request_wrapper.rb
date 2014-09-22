eb959f6617d30f74278b5e458056b76162cb39f1
Nathan Stitt <nathan@stitt.org>
2014-09-21 19:39:52 -0500

    remove testing code that defaulted user

--- lib/skr/api/request_wrapper.rb
+++ lib/skr/api/request_wrapper.rb
@@ -4,15 +4,13 @@ module Skr
 
 
             def perform_request(model)
-                env['rack.session'][:user_id]=1 # FIXME - REMOVE THIS WHEN DONE TESTING!
-
                 authentication = Skr::API.config.authentication_provider.new(environment:env, params:params)
 
                 unless authentication.allowed_access_to?(model, request.request_method )
                     error!({ errors: {user: "Access Denied"}, message: authentication.error_message }, 401)
                 end
                 ::Skr::UserProxy.scoped_to(authentication.current_user) do | user |
-                    API.logger.info "#{user.id} (#{user.login}) Data: #{request.params.data.to_json}"
+                    Skr::Core.logger.debug  "User #{user.id} (#{user.login}) Data: #{request.params.data.to_json}"
                     response = yield authentication
                     status(406) if false == response
                     response
